992b3b9d19760bde0284c83357f3f90a5a1df635
77251688 <907624307@qq.com>
2022-11-21 13:14:53 +0800

    md5

---
+++ src/main/java/com/i77251680/crypto/md5/Md5Crypto.java
@@ -0,0 +1,28 @@
+package com.i77251680.crypto.md5;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Md5Crypto {
+    public static byte[] encrypt(String v) {
+        MessageDigest md5 = null;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+            md5.update(v.getBytes());
+            return md5.digest();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("MD5ERROR");
+        }
+    }
+
+    public static byte[] encryptbuf(byte[] v) {
+        MessageDigest md5 = null;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+            md5.update(v);
+            return md5.digest();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("MD5ERROR");
+        }
+    }
+}
