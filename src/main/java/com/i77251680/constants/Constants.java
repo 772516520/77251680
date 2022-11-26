99c16d908e23329847ce1b6181fd9ef215d10a8d
77251688 <907624307@qq.com>
2022-11-26 22:50:01 +0800

    constants

---
+++ src/main/java/com/i77251680/constants/Constants.java
@@ -0,0 +1,39 @@
+package com.i77251680.constants;
+
+public class Constants {
+    /**
+     * 0 length byte[]
+     */
+    public static final byte[] BUF0 = new byte[0];
+
+    /**
+     * 0 length byte[]
+     */
+    public static final byte[] BUF1 = new byte[1];
+
+    /**
+     * 4 length byte[]
+     */
+    public static final byte[] BUF4 = new byte[4];
+
+    /**
+     * 4 length byte[]
+     */
+    public static final byte[] BUF6 = new byte[6];
+
+    /**
+     * 8 length byte[]
+     */
+    public static final byte[] BUF8 = new byte[8];
+
+    /**
+     * 16 length byte[]
+     */
+    public static final byte[] BUF16 = new byte[16];
+
+    /**
+     * login sso buf
+     */
+    public static final byte[] LoginBUF = new byte[]{0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00};
+
+}
