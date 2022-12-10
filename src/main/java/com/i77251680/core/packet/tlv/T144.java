97b720e4f9509b336a242207365b7973d8c07535
77251688 <907624307@qq.com>
2022-12-10 23:00:28 +0800

    简化形参

--- src/main/java/com/i77251680/core/packet/tlv/T144.java
+++ src/main/java/com/i77251680/core/packet/tlv/T144.java
@@ -9,25 +9,16 @@ import java.io.IOException;
 
 public class T144 {
     public static Writer writeT144(
-            String imei,
             FullDevice fullDevice,
-            String os_type,
-            String release,
-            String sim,
-            String apn,
-            String device,
-            byte[] guid,
-            String brand,
-            String model,
             byte[] tgtgt
     ) throws IOException {
         byte[] body = new Writer()
                 .writeShort(5)
-                .writeBytes(PackTlv.pack(0x109, T109.writeT109(imei)))
+                .writeBytes(PackTlv.pack(0x109, T109.writeT109(fullDevice.imei)))
                 .writeBytes(PackTlv.pack(0x52d, T52D.writeT52D(fullDevice)))
-                .writeBytes(PackTlv.pack(0x124, T124.writeT124(os_type, release, sim, apn)))
-                .writeBytes(PackTlv.pack(0x128, T128.writeT128(device, guid, brand)))
-                .writeBytes(PackTlv.pack(0x16e, T16E.writeT16E(model)))
+                .writeBytes(PackTlv.pack(0x124, T124.writeT124(fullDevice.os_type, fullDevice.version.release, fullDevice.sim, fullDevice.apn)))
+                .writeBytes(PackTlv.pack(0x128, T128.writeT128(fullDevice.model, fullDevice.guid, fullDevice.brand)))
+                .writeBytes(PackTlv.pack(0x16e, T16E.writeT16E(fullDevice.model)))
                 .read();
         return new Writer()
                 .writeBytes(new Tea().encrypt(body, tgtgt));
