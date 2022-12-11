9da9a300615f6074612789ea32921426458a1eb7
77251688 <907624307@qq.com>
2022-12-11 23:05:33 +0800

    fix

--- src/main/java/com/i77251680/crypto/ecdh/Ecdh.java
+++ src/main/java/com/i77251680/crypto/ecdh/Ecdh.java
@@ -1,6 +1,7 @@
 package com.i77251680.crypto.ecdh;
 
 import com.i77251680.crypto.md5.Md5Crypto;
+import com.i77251680.utils.ArrayUtils;
 import com.i77251680.utils.HexFormat;
 
 import javax.crypto.KeyAgreement;
@@ -12,27 +13,50 @@ import java.util.Arrays;
 
 public class Ecdh {
     private final String i77251680_PUBLIC_KEY = "04EBCA94D733E399B2DB96EACDD3F69A8BB0F74224E2B44E3357812211D2E62EFBC91BB553098E25E33A799ADC7F76FEB208DA7C6522CDB0719A305180CC54A82E";
-    private final byte[] byteKey = HexFormat.of().parseHex(i77251680_PUBLIC_KEY);
+    private final byte[] X509_PREFIX = HexFormat.of().parseHex("3059301306072A8648CE3D020106082A8648CE3D030107034200");
+    private final byte[] byteKey = ArrayUtils.concat(X509_PREFIX, HexFormat.of().parseHex(i77251680_PUBLIC_KEY));
+    private final PublicKey key;
+    private final KeyPairGenerator clientPair;
+    KeyPair keyPair;
+
+    public final byte[] publicKey;
     public final byte[] shareKey;
 
     public Ecdh() {
-        this.shareKey = generateShareKey();
-    }
-
-    public byte[] generateShareKey() {
         try {
-            PublicKey key = KeyFactory.getInstance("EC")
+            key = KeyFactory.getInstance("EC")
                     .generatePublic(new X509EncodedKeySpec(byteKey));
-            KeyPairGenerator clientPair = KeyPairGenerator.getInstance("ECDH");
-            clientPair.initialize(new ECGenParameterSpec("prime256v1"));
-            KeyAgreement shareKeyAgree = KeyAgreement.getInstance("ECDH");
-            shareKeyAgree.init(clientPair.generateKeyPair().getPrivate());
-            shareKeyAgree.doPhase(key, true);
-            return Md5Crypto.encryptbuf(Arrays.copyOf(shareKeyAgree.generateSecret(), 16));
-        } catch (NoSuchAlgorithmException | InvalidAlgorithmParameterException | InvalidKeyException |
-                 InvalidKeySpecException e) {
+        } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
+        try {
+            clientPair = KeyPairGenerator.getInstance("EC");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            clientPair.initialize(new ECGenParameterSpec("secp256r1"));
+        } catch (InvalidAlgorithmParameterException e) {
+            throw new RuntimeException(e);
+        }
+        keyPair = clientPair.genKeyPair();
+        this.shareKey = generateShareKey();
+        this.publicKey = generatePublicKey();
+    }
+
+    private byte[] generateShareKey() {
+        try {
+            KeyAgreement shareKeyAgree = KeyAgreement.getInstance("ECDH");
+            shareKeyAgree.init(keyPair.getPrivate());
+            shareKeyAgree.doPhase(key, true);
+            return Md5Crypto.encrypt(Arrays.copyOf(shareKeyAgree.generateSecret(), 16));
+        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private byte[] generatePublicKey() {
+        return Arrays.copyOfRange(keyPair.getPublic().getEncoded(), 26, 91);
     }
 
 }
