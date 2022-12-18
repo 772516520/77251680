d2d288da81dac73ce8920cd9ed9307fe2e604fff
77251688 <907624307@qq.com>
2022-12-18 18:32:23 +0800

    loginType

---
+++ src/main/java/com/i77251680/entity/enums/LoginType.java
@@ -0,0 +1,7 @@
+package com.i77251680.entity.enums;
+
+public class LoginType {
+    public static final int PASSWORD = 1;
+    public static final int SMS = 3;
+    public static final int WE_CHAT = 4;
+}
