6c88124b13a7eebbe6c25f10470a44ffe836d546
77251688 <907624307@qq.com>
2022-12-23 21:40:21 +0800

    jce

---
+++ src/main/java/com/i77251680/entity/Jce/JceElement.java
@@ -0,0 +1,11 @@
+package com.i77251680.entity.Jce;
+
+public class JceElement {
+    public Object tag;
+    public Object value;
+
+    public JceElement(Object tag, Object value) {
+        this.tag = tag;
+        this.value = value;
+    }
+}
---
+++ src/main/java/com/i77251680/entity/Jce/JceHead.java
@@ -0,0 +1,11 @@
+package com.i77251680.entity.Jce;
+
+public class JceHead {
+    public Object tag;
+    public Object type;
+
+    public JceHead(Object tag, Object type) {
+        this.tag = tag;
+        this.type = type;
+    }
+}
