97e6395d03849e9411d0ba6b7640ea63d60bd5eb
77251688 <907624307@qq.com>
2022-12-24 16:48:39 +0800

    DecodeT119

---
+++ src/main/java/com/i77251680/network/protocol/packet/unpack/tlv/DecodeT119.java
@@ -0,0 +1,33 @@
+package com.i77251680.network.protocol.packet.unpack.tlv;
+
+import com.i77251680.core.client.Sig;
+import com.i77251680.crypto.md5.Md5Crypto;
+import com.i77251680.crypto.tea.Tea;
+import com.i77251680.utils.ArrayUtils;
+import io.netty.buffer.ByteBuf;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import static io.netty.buffer.Unpooled.wrappedBuffer;
+
+public class DecodeT119 {
+    public static void decode(ByteBuf t119) {
+        ByteBuf r = wrappedBuffer(new Tea().decrypt(t119.array(), Sig.tgtgt));
+        r.readShort();
+        Map<Integer, ByteBuf> t = ReadTlv.read(r);
+        Sig.tgt = t.get(0x10a).array();
+        Sig.skey = t.get(0x120).array();
+        Sig.d2 = t.get(0x143).array();
+        Sig.d2key = t.get(0x305).array();
+        Sig.tgtgt = Md5Crypto.encrypt(Sig.d2key);
+        byte[] token = ArrayUtils.concat(Sig.d2key, Sig.d2, Sig.tgt);
+        ByteBuf t11a = t.get(0x11a);
+        byte[] t11a_ = t11a.array();
+        t11a.readShort();
+        int age = t11a.readByte();
+        int gender = t11a.readByte();
+        String nickname = new String(Arrays.copyOfRange(t11a_, 5, t11a_.length));
+        System.out.println(nickname);
+    }
+}
