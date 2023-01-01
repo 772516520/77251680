6e0d8844017e051bbbb606ec430f32dd35f57cc4
77251688 <907624307@qq.com>
2023-01-01 13:29:38 +0800

    OnlineStatus

---
+++ src/main/java/com/i77251680/entity/enums/OnlineStatus.java
@@ -0,0 +1,31 @@
+package com.i77251680.entity.enums;
+
+/**
+ * 在线状态
+ */
+public class OnlineStatus {
+    /**
+     * 在线
+     */
+    public static final int Online = 11;
+    /**
+     * 离开
+     */
+    public static final int Absent = 31;
+    /**
+     * 隐身
+     */
+    public static final int Invisible = 41;
+    /**
+     * 忙碌
+     */
+    public static final int Busy = 50;
+    /**
+     * Qme
+     */
+    public static final int Qme = 60;
+    /**
+     * 请勿打扰
+     */
+    public static final int DontDisturb = 70;
+}
