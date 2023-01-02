a41518f1ee1f11f3e87b2e6753419506f91c7d88
77251688 <907624307@qq.com>
2023-01-02 16:00:51 +0800

    Platforms

---
+++ src/main/java/com/i77251680/entity/enums/Platforms.java
@@ -0,0 +1,13 @@
+package com.i77251680.entity.enums;
+
+import com.i77251680.entity.platform.APad;
+import com.i77251680.entity.platform.Android;
+import com.i77251680.entity.platform.IPad;
+import com.i77251680.entity.platform.Watch;
+
+public class Platforms {
+    public static final Android Android = new Android();
+    public static final APad aPad = new APad();
+    public static final IPad iPad = new IPad();
+    public static final Watch watch = new Watch();
+}
