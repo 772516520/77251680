c80917f154b5d8eb7029c22e8ea6be708eb06f2f
77251688 <907624307@qq.com>
2023-01-02 15:59:59 +0800

    enum LoginType

--- src/main/java/com/i77251680/entity/enums/LoginType.java
+++ src/main/java/com/i77251680/entity/enums/LoginType.java
@@ -1,7 +1,21 @@
 package com.i77251680.entity.enums;
 
-public class LoginType {
-    public static final int PASSWORD = 1;
-    public static final int SMS = 3;
-    public static final int WE_CHAT = 4;
+public enum LoginType {
+    PASSWORD(1, "PasswordLogin"), SMS(3, "SMSLogin"), WE_CHAT(4, "微信一键登录");
+    private final int type;
+    private final String remark;
+
+    LoginType(int type, String remark) {
+        this.type = type;
+        this.remark = remark;
+    }
+
+    public int getType() {
+        return this.type;
+    }
+
+    @Override
+    public String toString() {
+        return this.remark;
+    }
 }
