28025d649b8e3dde09fa62f04d324af542c40dab
77251688 <907624307@qq.com>
2023-01-10 21:42:43 +0800

    Gender

---
+++ src/main/java/com/i77251680/entity/enums/Gender.java
@@ -0,0 +1,21 @@
+package com.i77251680.entity.enums;
+
+public enum Gender {
+    male(1, "male"), female(0, "female"), unknown(2, "unknown");
+    private int sex;
+    private String remark;
+
+    Gender(int sex, String remark) {
+        this.sex = sex;
+        this.remark = remark;
+    }
+
+    public int getSex() {
+        return this.sex;
+    }
+
+    @Override
+    public String toString() {
+        return this.remark;
+    }
+}
