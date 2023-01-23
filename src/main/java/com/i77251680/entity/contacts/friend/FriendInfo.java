2010cac4af2771ca4b291052d7764dddffeab4f2
77251688 <907624307@qq.com>
2023-01-23 18:17:52 +0800

    FriendInfo

---
+++ src/main/java/com/i77251680/entity/contacts/friend/FriendInfo.java
@@ -0,0 +1,10 @@
+package com.i77251680.entity.contacts.friend;
+
+import com.i77251680.entity.contacts.StrangerInfo;
+import com.i77251680.entity.enums.Gender;
+
+public class FriendInfo extends StrangerInfo {
+    public Gender gender;
+    public String remark;
+    public int class_id;
+}
