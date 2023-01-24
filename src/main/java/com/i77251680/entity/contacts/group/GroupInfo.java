d4cf430431d3d57d7a22911172cf8981abdb367e
77251688 <907624307@qq.com>
2023-01-24 20:30:08 +0800

    GroupInfo

---
+++ src/main/java/com/i77251680/entity/contacts/group/GroupInfo.java
@@ -0,0 +1,19 @@
+package com.i77251680.entity.contacts.group;
+
+public class GroupInfo {
+    public long group_id;
+    public String group_name;
+    public int member_count;
+    public int max_member_count;
+    public long owner_id;
+    public boolean admin_flag;
+    public long last_join_time;
+    public long last_sent_time;
+    public long shutUp_time_whole;
+    public long shutUp_time_me;
+    public long create_time;
+    public int grade;
+    public int max_admin_count;
+    public int active_member_count;
+    public long update_time;
+}
