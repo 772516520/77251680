c7936df188250681499827a04fffe6e9f39cc265
77251688 <907624307@qq.com>
2023-01-30 20:13:45 +0800

    Contact

---
+++ src/main/java/com/i77251680/contact/Contact.java
@@ -0,0 +1,7 @@
+package com.i77251680.contact;
+
+public interface Contact {
+    String getAvatarUrl();
+
+    String getAvatarUrl(int size);
+}
---
+++ src/main/java/com/i77251680/contact/Contactable.java
@@ -0,0 +1,9 @@
+package com.i77251680.contact;
+
+import com.i77251680.App;
+
+public abstract class Contactable {
+    protected Contactable(App client) {
+
+    }
+}
