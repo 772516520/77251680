eada2590fb0b60f3de3a758916e9e86f7182d2b4
77251688 <907624307@qq.com>
2023-03-19 20:48:17 +0800

    delete

--- src/main/java/com/i77251680/core/codec/protobuf/pbpack/PackProtobuf.java
+++
@@ -1,9 +0,0 @@
-package com.i77251680.core.codec.protobuf.pbpack;
-
-import java.io.IOException;
-
-public interface PackProtobuf {
-    byte[] pack() throws IOException;
-
-    byte[] build() throws IOException;
-}
--- src/main/java/com/i77251680/core/codec/protobuf/pbpack/UnpackProtobuf.java
+++
@@ -1,5 +0,0 @@
-package com.i77251680.core.codec.protobuf.pbpack;
-
-public interface UnpackProtobuf {
-    void unpack(byte[] packet);
-}
