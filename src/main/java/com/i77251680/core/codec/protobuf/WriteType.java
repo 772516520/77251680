b7bc0a7e9eabbad2481f6416aea93ac70e55b603
77251688 <907624307@qq.com>
2023-03-22 23:44:44 +0800

    fixed32 type

--- src/main/java/com/i77251680/core/codec/protobuf/WriteType.java
+++ src/main/java/com/i77251680/core/codec/protobuf/WriteType.java
@@ -3,4 +3,5 @@ package com.i77251680.core.codec.protobuf;
 class WriteType {
     public static final int VARINT = 0;
     public static final int BYTES = 2;
+    public static final int FIXED32 = 5;
 }
