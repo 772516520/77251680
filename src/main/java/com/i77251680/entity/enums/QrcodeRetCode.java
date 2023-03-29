40804d46e1e6c4d368ef2bdab837cb9260fbfabb
77251688 <907624307@qq.com>
2023-03-29 22:32:47 +0800

    retcode

---
+++ src/main/java/com/i77251680/entity/enums/QrcodeRetCode.java
@@ -0,0 +1,9 @@
+package com.i77251680.entity.enums;
+
+public class QrcodeRetCode {
+    public static final int OtherError = 0;
+    public static final int Timeout = 0x11;
+    public static final int WaitingForScan = 0x30;
+    public static final int WaitingForConfirm = 0x35;
+    public static final int Canceled = 0x36;
+}
