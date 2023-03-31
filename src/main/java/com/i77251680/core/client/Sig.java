d7c27750f96c842b1b16e9aabfb60a197dbdb8d6
77251688 <907624307@qq.com>
2023-03-31 22:31:07 +0800

    rename

--- src/main/java/com/i77251680/core/client/Sig.java
+++ src/main/java/com/i77251680/core/client/Sig.java
@@ -8,15 +8,16 @@ import static io.netty.buffer.Unpooled.wrappedBuffer;
 
 public class Sig {
     public static byte[] d2 = Constants.BUF0;
-    public static byte[] d2key = Constants.BUF0;
-    public static byte[] skey = Constants.BUF0;
+    public static byte[] d2Key = Constants.BUF0;
+    public static byte[] sKey = Constants.BUF0;
     public static byte[] tgt = Constants.BUF0;
     public static byte[] t104 = Constants.BUF0;
     public static byte[] t174 = Constants.BUF0;
     public static byte[] t402 = Constants.BUF0;
     public static byte[] t403 = Constants.BUF0;
-    public static byte[] G = Constants.BUF0;
+    public static byte[] g = Constants.BUF0;
     public static byte[] qrsig = Constants.BUF0;
+    public static long timeDiff;
     public static byte[] publicKey;
     public static byte[] shareKey;
     public static byte[] tgtgt = Random.randomBytes(16);
