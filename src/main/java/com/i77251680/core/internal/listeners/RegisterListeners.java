1a4e9c9e796e70e1415e8969989028b6956aa33d
77251688 <907624307@qq.com>
2023-04-03 23:56:50 +0800

    RegisterListener

--- src/main/java/com/i77251680/core/internal/listeners/RegisterListeners.java
+++ src/main/java/com/i77251680/core/internal/listeners/RegisterListeners.java
@@ -1,6 +1,8 @@
 package com.i77251680.core.internal.listeners;
 
 import com.i77251680.Client;
+import com.i77251680.handler.PrivateMessageHandler;
+import com.i77251680.network.protocol.packet.decoder.PbGetMsgResponse;
 
 public class RegisterListeners {
     public static void register(Client client) {
@@ -10,7 +12,10 @@ public class RegisterListeners {
         SliderListener.listen(client);
         QrcodeListener.listen(client);
         QrcodeErrorListener.listen(client);
+        KickoffListener.listen(client);
         SSOListener.listen(client);
-//        MessageListeners.listen(client);
+        MessageListeners.listen(client);
+        PbGetMsgResponse.register(client);
+        PrivateMessageHandler.handle(client);
     }
 }
