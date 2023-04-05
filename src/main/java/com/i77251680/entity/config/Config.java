74c4a0d10689ad5fd7a546cff3d6fdfd0f4a2edc
77251688 <907624307@qq.com>
2023-04-05 22:53:44 +0800

    Config

--- src/main/java/com/i77251680/entity/config/Config.java
+++ src/main/java/com/i77251680/entity/config/Config.java
@@ -2,25 +2,23 @@ package com.i77251680.entity.config;
 
 import com.i77251680.entity.enums.Platform;
 
-public class Config {
-    public Platform platform;
-    public int hb480_interval = 30000;
+import java.io.File;
 
-    public Config() {
-        platform = Platform.Android;
+public class Config {
+    public Platform platform = Platform.Android;
+    public boolean auto_server = true;
+    public boolean ignore_self = true;
+    public boolean resend = true;
+    public boolean cache_group_member = true;
+    public int reconn_interval = 5;
+    public int hb480_interval = 30 * 1000;
+    public File data_dir = new File(".");
+    public static Config DEFAULT = new Config();
+
+    private Config() {
     }
 
     public Config(Platform platform) {
         this.platform = platform;
-    }
-
-    public Config(int hb480_interval) {
-        platform = Platform.Android;
-        this.hb480_interval = hb480_interval * 1000;
-    }
-
-    public Config(Platform platform, int hb480_interval) {
-        this.platform = platform;
-        this.hb480_interval = hb480_interval * 1000;
     }
 }
