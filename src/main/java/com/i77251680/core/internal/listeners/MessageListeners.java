65f36eebbb569ff2949b50c90bdfc35effa0f06c
77251688 <907624307@qq.com>
2023-04-12 21:47:02 +0800

    package

--- src/main/java/com/i77251680/core/internal/listeners/MessageListeners.java
+++ src/main/java/com/i77251680/core/internal/listeners/MessageListeners.java
@@ -2,16 +2,17 @@ package com.i77251680.core.internal.listeners;
 
 import com.i77251680.Client;
 import com.i77251680.core.codec.jce.Jce;
-import com.i77251680.entity.packet.sso.SSO;
 import com.i77251680.event.EventListener;
 import com.i77251680.handler.MessageHandler;
+import com.i77251680.network.protocol.packet.Packet;
 
 public class MessageListeners {
     private MessageListeners(Client client) {
-        EventListener.on("internal.sso", (SSO sso) -> {
-            String cmd = sso.cmd;
-            int seq = sso.seq;
-            byte[] payload = sso.payload;
+        EventListener.on("internal.sso", (Packet packet) -> {
+            String cmd = packet.cmd;
+            int seq = packet.seq;
+            if (!cmd.equals("MessageSvc.PushNotify")) return;
+            byte[] payload = packet.payload;
             int type = (int) Jce.decodeWrapper(payload).get(5);
             switch (type) {
                 case 33: //群员入群
@@ -23,6 +24,7 @@ public class MessageListeners {
                 case 208: //好友语音
                 case 529: //离线文件
                     MessageHandler.handleMessage(client);
+                    break;
                 case 84: //群请求
                 case 87: //群邀请
                 case 525: //群请求(来自群员的邀请)
