7f9711f12dda066027529906377ca038cc67d910
77251688 <907624307@qq.com>
2023-04-16 23:07:44 +0800

    getter setter

--- src/main/java/com/i77251680/entity/contacts/StrangerInfo.java
+++ src/main/java/com/i77251680/entity/contacts/StrangerInfo.java
@@ -3,4 +3,24 @@ package com.i77251680.entity.contacts;
 public class StrangerInfo {
     public long user_id;
     public String nickname;
+
+    protected StrangerInfo() {
+    }
+
+    private StrangerInfo(long user_id, String nickname) {
+        this.user_id = user_id;
+        this.nickname = nickname;
+    }
+
+    public void set(long user_id, String nickname) {
+        new StrangerInfo(user_id, nickname);
+    }
+
+    public long getUser_id() {
+        return user_id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
 }
--- src/main/java/com/i77251680/entity/contacts/friend/FriendInfo.java
+++ src/main/java/com/i77251680/entity/contacts/friend/FriendInfo.java
@@ -4,7 +4,36 @@ import com.i77251680.entity.contacts.StrangerInfo;
 import com.i77251680.entity.enums.Gender;
 
 public class FriendInfo extends StrangerInfo {
-    public Gender gender;
-    public String remark;
-    public int class_id;
+    private int class_id;
+    private Gender gender;
+    private String remark;
+    private String nickname;
+
+    private FriendInfo(int class_id, Gender gender, String remark, String nickname) {
+        super();
+        this.gender = gender;
+        this.remark = remark;
+        this.class_id = class_id;
+        this.nickname = nickname;
+    }
+
+    public static FriendInfo set(int class_id, Gender gender, String remark, String nickname) {
+        return new FriendInfo(class_id, gender, remark, nickname);
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public int getClass_id() {
+        return class_id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
 }
