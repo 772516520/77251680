5abf4d2ec8976c326fd8ee14f45629acebe66579
77251688 <907624307@qq.com>
2023-04-18 22:31:10 +0800

    highway

---
+++ src/main/java/com/i77251680/network/highway/BigData.java
@@ -0,0 +1,8 @@
+package com.i77251680.network.highway;
+
+public class BigData {
+    public static String ip;
+    public static int port;
+    public static byte[] sig_session;
+    public static byte[] session_key;
+}
