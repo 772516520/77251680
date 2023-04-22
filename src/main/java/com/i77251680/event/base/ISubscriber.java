7f3303f3275aaf4ff9be6586bb6d7345477d4054
77251688 <907624307@qq.com>
2023-04-22 21:12:53 +0800

    delete

--- src/main/java/com/i77251680/event/base/ISubscriber.java
+++
@@ -1,5 +0,0 @@
-package com.i77251680.event.base;
-
-public interface ISubscriber<T> {
-    void onReceive(String eventName, T data);
-}
--- src/main/java/com/i77251680/event/base/Receive.java
+++
@@ -1,6 +0,0 @@
-package com.i77251680.event.base;
-
-@FunctionalInterface
-public interface Receive<T> {
-    void receive(T data);
-}
